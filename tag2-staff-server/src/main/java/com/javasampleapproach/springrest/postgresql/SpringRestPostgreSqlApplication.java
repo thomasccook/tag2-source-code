490be764f00471089bf689a1c2c18a5878bc73d6
Thomas Cook <thomascook@Thomass-iMac.hsd1.ut.comcast.net>
2019-06-07 00:51:38 -0600

    Initial check-in of tag2.

---
+++ tag2-staff-server/src/main/java/com/javasampleapproach/springrest/postgresql/SpringRestPostgreSqlApplication.java
@@ -0,0 +1,14 @@
+package com.javasampleapproach.springrest.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// https://grokonez.com/spring-framework/spring-data/spring-boot-angular-6-example-spring-data-rest-postgresql-example
+@SpringBootApplication
+public class SpringRestPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestPostgreSqlApplication.class, args);
+	}
+}
